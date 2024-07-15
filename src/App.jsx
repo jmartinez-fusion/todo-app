22ed57489ebaa7537532d51ea72466dcd5ac04f2
javi-mb <jh.martinezblanco@gmail.com>
2024-07-15 17:34:06 -0300

    new test

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,15 @@ function App() {
 
   useEffect(() => {
     document.title = `Count is ${count}`;
-  });
+    // Error: Falta la dependencia "count" en el array de dependencias
+  }, []);
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={viteLogo} className="logo" />
+          {/* Error: Falta la propiedad "alt" en la etiqueta img */}
         </a>
         <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
